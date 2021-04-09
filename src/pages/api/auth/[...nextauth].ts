ecdce8b92d71f1909816616d14c8cc174450b040
abauruel <abauruel@gmail.com>
2021-04-09 12:39:38 -0300

    feat: :sparkles: add stripe integration and faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
+
 import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -12,4 +15,37 @@ export default NextAuth({
  
   ],
 
+  callbacks:{
+      async signIn(user, account, profile){
+      try {
+
+        await fauna.query(
+           q.If(
+               q.Not(
+                   q.Exists(
+                       q.Match(
+                           q.Index('userByEmail'),
+                           q.Casefold(user.email)
+                       )
+                   )
+               ),
+               q.Create(
+                q.Collection('users'),{
+                    data: { email: user.email}
+                }
+            ),q.Get(
+                q.Match(
+                    q.Index('userByEmail'),
+                    q.Casefold(user.email)
+                )
+            )
+           )
+        )
+        return true
+      } catch (error) {
+          return false
+      }
+      
+      }
+  }
 })
