1d76e5602e6e488a206dac118c7d5361a340a0fc
abauruel <abauruel@gmail.com>
2021-04-15 16:54:53 -0300

    feat: add post, prismic cms

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
+import { Session } from 'next-auth'
 import { signIn, useSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
 import { api } from '../../services/api'
 import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
@@ -6,13 +8,22 @@ import styles from './styles.module.scss'
 interface SubscribeButtonProps{
     priceId: string,
 }
+
+
+
 export function SubscribeButton({priceId}: SubscribeButtonProps){
     const [session] = useSession()
-
+    const router = useRouter()
+    
     async function handleSubscribe(){
         if(!session){
             signIn('gihub')
         return
+        }
+
+        if(session?.activeSubscription){
+            router.push('/posts');
+            return
         }
 
         try{
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,13 @@
-import NextAuth from 'next-auth'
+import NextAuth, { Session } from 'next-auth'
 
 import Providers from 'next-auth/providers'
 import { query as q } from 'faunadb'
 import { fauna } from '../../../services/fauna'
+import { session } from 'next-auth/client'
+
+interface SessionProps extends Session{
+    activeSubscription: unknown
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -16,6 +21,46 @@ export default NextAuth({
   ],
 
   callbacks:{
+      async session(session){
+         try{
+             
+            const userActiveSubscription = await fauna.query(
+
+                q.Get(
+                   q.Intersection([
+                      q.Match(
+                          q.Index('subscription_ByUserRef'),
+                          q.Select(
+                              "ref",
+                              q.Get(
+                                  q.Match(
+                                      q.Index('userByEmail'),
+                                      q.Casefold(session.user.email)
+                                  )
+                              )
+                          )
+                      ),
+                      q.Match(
+                          q.Index('subscriptionByActive'),
+                          "active"
+                      )]
+
+
+                      )
+                )
+            )
+            return {
+                ...session,
+                activeSubscription: userActiveSubscription
+            }
+         }catch{
+             return{
+                 ...session,
+                 activeSubscription: null
+             }
+         }
+      },
+
       async signIn(user, account, profile){
       try {
 
